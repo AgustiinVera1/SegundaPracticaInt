0b0699742a30528376231ddc4ffd9ac9609827e2
Agustin Vera <veraagustin01@hotmail.com>
2023-10-06 12:34:29 -0300

    no puedo hacer para que se actualize el nuevo producto agregado sin tener que recargar la pagina, en realTimeProducts

---
+++ src/routes/views.router.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import { manager1 } from '../ProductManager.js';
+
+const router = Router();
+
+router.get('/home', async (req, res) => {
+    try {
+        const products = await manager1.getProducts({});
+        res.render('home', { listProducts: products });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get('/realtimeproducts', async (req, res) => {
+    try {
+        const products = await manager1.getProducts({});
+        res.render('realTimeProducts', { listProducts: products });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+export default router;
